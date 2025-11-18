7ff8f02c57fd757212027262d992b911447f0e64
Caleb Lawson <Calebmlawson@gmail.com>
2025-11-18 11:51:00 -0600

    tempate update

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   // Configuration for blog template consumer
   output: 'standalone',
   transpilePackages: ["@caleblawson/blog-shell"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
